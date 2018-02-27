667d73b12688eadb9ef2f67b6013257a2084ffc3
GateauXD <dantran2016@gmail.com>
2018-02-26 17:43:23 -0800

    Added server connection

--- FutureFirst/app/src/main/java/com/example/gateauxd/futurefirst/Login.java
+++ FutureFirst/app/src/main/java/com/example/gateauxd/futurefirst/Login.java
@@ -1,7 +1,11 @@
-package com.example.gateauxd.futurefirst;
 
+package com.example.gateauxd.futurefirst;
+import java.net.Socket;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +13,35 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
     }
+    public void signUp(View view){
+        Intent startNewActivity = new Intent(this, signUp.class);
+        startActivity(startNewActivity);
+    }
+    Client socket = new Client("192.168.0.8", 1234);
+    socket.setClientCallback(new Client.ClientCallback () {
+        @Override
+        public void onMessage(String message) {
+        }
+
+        @Override
+        public void onConnect(Socket socket) {
+            socket.send("Hello World!\n");
+            socket.disconnect();
+        }
+
+        @Override
+        public void onDisconnect(Socket socket, String message) {
+        }
+
+        @Override
+        public void onConnectError(Socket socket, String message) {
+        }
+    });
+
+socket.connect();
 }
+
+
+
