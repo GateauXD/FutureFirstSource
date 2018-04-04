1d5816c2143b8daa855d3e429580c53898c3b059
GateauXD <dantran2016@gmail.com>
2018-04-03 22:06:49 -0700

    Added Profile Picture

---
+++ FutureFirst/app/src/main/java/com/example/gateauxd/futurefirst/ProfilePage.java
@@ -0,0 +1,12 @@
+package com.example.gateauxd.futurefirst;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProfilePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
