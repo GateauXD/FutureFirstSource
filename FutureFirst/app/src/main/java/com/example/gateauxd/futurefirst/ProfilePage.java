a927947244d2d3aa9516bd780b77838d65075c69
GateauXD <dantran2016@gmail.com>
2018-04-14 14:14:51 -0700

    Added settings pages.

--- FutureFirst/app/src/main/java/com/example/gateauxd/futurefirst/ProfilePage.java
+++ FutureFirst/app/src/main/java/com/example/gateauxd/futurefirst/ProfilePage.java
@@ -7,6 +7,12 @@ public class ProfilePage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_page);
+    }
+    public void profileSettings(android.view.View view){
+        android.content.Intent startNewActivity = new android.content.Intent(this, profileSettings.class);
+        startActivity(startNewActivity);
     }
 }
